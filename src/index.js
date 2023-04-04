e2ca3464db6501976487a580941571c81a6131db
kalbek <access2kaleb@gmail.com>
2023-04-04 16:03:28 +0300

    copied files from To-do List project

---
+++ src/index.js
@@ -0,0 +1,48 @@
+// import _ from 'lodash';
+import './style.css';
+import Todo from './modules/Todo.js';
+
+const addTodo = document.querySelector('#enter');
+const todo = document.querySelector('#todo-input');
+const reset = document.querySelector('#reset');
+
+function addTodos() {
+  if (todo.value !== '') Todo.setTodo(todo.value, false);
+  todo.value = '';
+  todo.focus();
+}
+
+function component() {
+  const element = document.createElement('div');
+
+  // handle add todo's on click
+  addTodo.addEventListener('click', () => {
+    addTodos();
+  });
+  // handle add todo's on Enter key pressed
+  todo.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') addTodos();
+  });
+  // handle reset todos
+  reset.addEventListener('click', () => {
+    Todo.resetTodos();
+  });
+
+  return element;
+}
+
+window.onload = () => {
+  // update todoList on page load
+  const currentTodo = localStorage.getItem('todo');
+  // initially since there is no todo's donot update ui from local storage
+  if (currentTodo === null) {
+    Todo.todoList = Todo.todoList.filter((todo) => todo.index === -1);
+    Todo.updateUI(document.querySelector('.list-container'));
+  } else {
+    // but if todos exist, update UI with local storage
+    Todo.todoList = JSON.parse(currentTodo);
+    Todo.updateUI(document.querySelector('.list-container'));
+  }
+  todo.focus();
+};
+document.body.appendChild(component());
---
+++ src/modules/test.test.js
@@ -0,0 +1,9 @@
+// import Todo from './Todo';
+// const sayHi = Todo.hi
+
+// const Todo = require('./Todo')
+import Todo from './Todo'
+
+// test("adds 1 + 2 to equal 3", () => {
+//   expect(sayHi(1, 2)).toBe(3);
+// });
